2c68bb013b21ef326bfa352f252ae96f02793525
Colby <ironflame24@yahoo.com>
2018-02-03 14:12:57 -0700

    Got everything synced up

---
+++ acme/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>ACME |</title>
+  <meta name="description" content="Template file">
+  <meta name="author" content="Colby">
+
+  <link rel="stylesheet" href="css/acmeStyle.css">
+</head>
+
+<body>
+    
+    <main class="mainContainer">
+    <header>
+    <div id="logo">
+        <img src="images/site/logo.png" alt="Logo">
+    </div>
+    
+    <nav>
+        <noscript>Javasript is needed for this site to function. Either enable it or switch to a real browser.</noscript>
+        <ul>
+            <li><a id="listI0" href="index.php">Home</a></li>    
+            <li><a id="listI1" href="#"></a></li>
+            <li><a id="listI2" href="#"></a></li>
+            <li><a id="listI3" href="#"></a></li>
+            <li><a id="listI4" href="#"></a></li>
+        </ul>
+    </nav>
+        </header>
+    <h1 id="pageTitle"></h1>
+    
+    <div id="homeContent">
+        <div class="homeImage">
+            
+            <section class="homeHolder">
+                <div id="adaptHome">  
+                    <img id="rocket" src="images/site/feature.jpg" alt="Featured Product">   
+                    <article><h1>The RoadRunner Rocket</h1><h3>Highly Efficient!<br>Built-in Targeting System!<br>Launch pad included!</h3></article>
+                    <img class="rocketButton" src="images/site/iwantit.gif" alt="Purchase button">
+                </div>
+            </section>
+            
+            
+        </div>
+        <section class="lowerHome">
+            
+            <div class="featuredRecipes">
+            <h3>Featured Recipes</h3>
+                <div id="imageAligner">
+                    <img src="images/recipes/bbqsand.jpg" alt="BBQ">
+                    <img src="images/recipes/potpie.jpg" alt="PotPie">
+                </div>
+                <a id="touchThat">
+                    <div>Roadrunner BBQ</div>
+                    <div>Roadrunner Pot Pie</div>
+                </a>
+                <div id="imageAligner1">
+                    <img src="images/recipes/soup.jpg" alt="Soup!">
+                    <img src="images/recipes/taco.jpg" alt="Taco">
+                </div>
+                <a id="touchThat1">
+                    <div>Roadrunner Soup</div>
+                    <div>Roadrunner Tacos</div>
+                </a>
+                
+            </div>
+            <div class="rocketReviews">
+            <h3>Rocket Reviews</h3>
+                <ul>
+                    <li>Crazy fast! Cut my hunting time down by 2 hours! <b>10/10</b></li>
+                    <li>Didn't suck as much as I thought it would. <b>7/10</b></li>
+                    <li>Ran great! All the features included were perfect! <b>9/10</b></li>
+                    <li>Totally acurate! Hit 9/10 times! <b>9/10</b></li>
+                    <li>Easy to use, worked smothly. <b>10/10</b></li>
+                </ul>
+            </div>
+        
+        
+        </section>
+        
+    </div>
+        <section class="productContent">
+            
+            <div id="productPic"></div>
+            
+            <div id="alingment">
+                <div id="productDesc"></div>
+                <div id="productReviews"></div>
+                <div id="productMan"></div>
+                <div id="productPrice"></div>
+                
+            </div>
+            
+                    
+        </section>
+        
+    <div id="mainContent"></div>
+    <footer>
+        &copy;2017 ACME Inc., All rights reserved.
+        <br/>
+        All images used are belived to be in "Fair Use". Please notify the Webmaster if any are not and they will be removed.
+    </footer>
+    
+    </main>
+    
+    
+   <script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>    
+  <script src="js/acme.js"></script>
+</body>
+</html>
---
+++ acme/view/500.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>ACME Template</title>
+  <meta name="description" content="Template file">
+  <meta name="author" content="Colby">
+
+  <link rel="stylesheet" href="../css/acmeStyle.css">
+</head>
+
+<body>
+    
+    <section class="mainContainer">
+    
+    <div id="logo">
+        <img src="../images/site/logo.png" alt="Logo">
+    </div>
+    
+    <nav>
+        <noscript>Javasript is needed for this site to function. Either enable it or switch to a real browser.</noscript>
+        <ul>
+            <li><a id="listI0" href="template.php">Home</a></li>    
+            <li><a id="listI1" href="#"></a></li>
+            <li><a id="listI2" href="#"></a></li>
+            <li><a id="listI3" href="#"></a></li>
+            <li><a id="listI4" href="#"></a></li>
+        </ul>
+    </nav>
+    
+    <h1 id="pageTitle">Normally I'd say, </h1>
+    <h2> But it looks like you were unable to connect to the server!</h2>
+    <div id="homePage"></div>
+    <div id="mainContent"></div>
+    <footer>
+        &copy;2017 ACME Inc., All rights reserved.
+        <br/>
+        All images used are belived to be in "Fair Use". Please notify the Webmaster if any are not and they will be removed.
+    </footer>
+    
+    </section>
+    
+    
+   <script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>    
+  <script src="js/acme.js"></script>
+</body>
+</html>
